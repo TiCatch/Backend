ee7af2d7611164e3b0c8217deb3938215f2eeffb
yj0111 <ooyouj@naver.com>
2025-01-12 22:45:21 +0900

    add: exception code 추가

--- src/main/java/TiCatch/backend/global/exception/ExceptionController.java
+++ src/main/java/TiCatch/backend/global/exception/ExceptionController.java
@@ -16,4 +16,39 @@ public class ExceptionController {
         log.info("Error Message : {}", err.getMessage());
         return ResponseResult.exceptionResponse(ExceptionCode.SERVER_EXCEPTION, err.getMessage());
     }
+
+    @ExceptionHandler(NotExistUserException.class)
+    public ResponseResult NotExistUserException(NotExistUserException err) {
+        log.info("Error : {}", err.getClass());
+        log.info("Error Message : {}", err.getMessage());
+        return ResponseResult.exceptionResponse(ExceptionCode.INVALID_REFRESH_TOKEN_EXCEPTION, err.getMessage());
+    }
+
+    @ExceptionHandler(ExpiredTokenException.class)
+    public ResponseResult ExpiredTokenException(ExpiredTokenException err) {
+        log.info("Error : {}", err.getClass());
+        log.info("Error Message : {}", err.getMessage());
+        return ResponseResult.exceptionResponse(ExceptionCode.EXPIRED_TOKEN_EXCEPTION, err.getMessage());
+    }
+
+    @ExceptionHandler(UnAuthorizedAccessException.class)
+    public ResponseResult UnAuthorizedAccessException(UnAuthorizedAccessException err) {
+        log.info("Error : {}", err.getClass());
+        log.info("Error Message : {}", err.getMessage());
+        return ResponseResult.exceptionResponse(ExceptionCode.UNAUTHORIZED_ACCESS_EXCEPTION, err.getMessage());
+    }
+
+    @ExceptionHandler(WrongTokenException.class)
+    public ResponseResult WrongTokenException(WrongTokenException err) {
+        log.info("Error : {}", err.getClass());
+        log.info("Error Message : {}", err.getMessage());
+        return ResponseResult.exceptionResponse(ExceptionCode.WRONG_TOKEN_EXCEPTION, err.getMessage());
+    }
+
+    @ExceptionHandler(InvalidRefreshTokenException.class)
+    public ResponseResult InvalidRefreshTokenException(InvalidRefreshTokenException err) {
+        log.info("Error : {}", err.getClass());
+        log.info("Error Message : {}", err.getMessage());
+        return ResponseResult.exceptionResponse(ExceptionCode.INVALID_REFRESH_TOKEN_EXCEPTION, err.getMessage());
+    }
 }
---
+++ src/main/java/TiCatch/backend/global/exception/UnAuthorizedAccessException.java
@@ -0,0 +1,12 @@
+package TiCatch.backend.global.exception;
+
+public class UnAuthorizedAccessException extends RuntimeException{
+    public UnAuthorizedAccessException() {
+        super(ExceptionCode.UNAUTHORIZED_ACCESS_EXCEPTION.getErrorMessage());
+    }
+
+    public UnAuthorizedAccessException(String message) {
+        super(message);
+    }
+
+}
