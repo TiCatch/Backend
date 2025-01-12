dcc6167d642904b2d8444cf626374d95a8905ecc
yj0111 <ooyouj@naver.com>
2025-01-12 22:44:18 +0900

    add: EnableJpaAuditing 추가

--- src/main/java/TiCatch/backend/BackendApplication.java
+++ src/main/java/TiCatch/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package TiCatch.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
 
