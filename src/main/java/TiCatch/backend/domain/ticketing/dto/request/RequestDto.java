d42cf50a933337f17885d1ca60ca38b527951d00
kitaee <dlrlxo999@naver.com>
2025-01-15 21:01:02 +0900

    del: 예시 dto 파일 삭제

--- src/main/java/TiCatch/backend/domain/ticketing/dto/request/RequestDto.java
+++
@@ -1,4 +0,0 @@
-package TiCatch.backend.domain.ticketing.dto.request;
-
-public class RequestDto {
-}
--- src/main/java/TiCatch/backend/domain/ticketing/dto/response/ResponseDto.java
+++
@@ -1,4 +0,0 @@
-package TiCatch.backend.domain.ticketing.dto.response;
-
-public class ResponseDto {
-}
