d6a27193c7c36a377591a16e9ba3bd71095acaf0
kitaee <dlrlxo999@naver.com>
2025-01-23 15:18:14 +0900

    add: NOT_IN_PROGRESS_TICKET_EXCEPTION 추가

--- src/main/java/TiCatch/backend/global/exception/ExceptionController.java
+++ src/main/java/TiCatch/backend/global/exception/ExceptionController.java
@@ -65,4 +65,11 @@ public class ExceptionController {
         log.info("Error Message : {}", err.getMessage());
         return ResponseResult.exceptionResponse(ExceptionCode.NOT_EXIST_TICKET_EXCEPTION, err.getMessage());
     }
+
+    @ExceptionHandler(NotInProgressTicketException.class)
+    public ResponseResult NotInProgressTicketException(NotInProgressTicketException err) {
+        log.info("Error : {}", err.getClass());
+        log.info("Error Message : {}", err.getMessage());
+        return ResponseResult.exceptionResponse(ExceptionCode.NOT_IN_PROGRESS_TICKET_EXCEPTION, err.getMessage());
+    }
 }
