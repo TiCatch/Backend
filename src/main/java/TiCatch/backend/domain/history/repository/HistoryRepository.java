f1388fb340134f65e945c07743b93301331241d2
yj0111 <ooyouj@naver.com>
2025-02-05 16:57:20 +0900

    fix: @Repository 추가

--- src/main/java/TiCatch/backend/domain/history/repository/HistoryRepository.java
+++ src/main/java/TiCatch/backend/domain/history/repository/HistoryRepository.java
@@ -2,6 +2,8 @@ package TiCatch.backend.domain.history.repository;
 
 import TiCatch.backend.domain.history.entity.History;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HistoryRepository extends JpaRepository<History, Long> {
 }
