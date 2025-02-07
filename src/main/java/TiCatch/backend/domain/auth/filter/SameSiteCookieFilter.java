fd197048dd4c8ec724570d54c3f6081134efd5e2
yj0111 <ooyouj@naver.com>
2025-02-07 18:36:00 +0900

    fix: same site cookie filter에 Partitioned 속성 추가

--- src/main/java/TiCatch/backend/domain/auth/filter/SameSiteCookieFilter.java
+++ src/main/java/TiCatch/backend/domain/auth/filter/SameSiteCookieFilter.java
@@ -1,23 +1,38 @@
 package TiCatch.backend.domain.auth.filter;
 
+import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import TiCatch.backend.domain.auth.dto.TokenDto;
 
 import java.io.IOException;
-import jakarta.servlet.Filter;
 
 @Component
+@RequiredArgsConstructor
 public class SameSiteCookieFilter implements Filter {
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        if (response instanceof HttpServletResponse httpServletResponse) {
-            httpServletResponse.setHeader("Set-Cookie",
-                    "refresh-token=; Path=/; HttpOnly; Secure; SameSite=None");
+        if (response instanceof HttpServletResponse httpServletResponse && request instanceof HttpServletRequest httpServletRequest) {
+
+            TokenDto tokenDto = (TokenDto) httpServletRequest.getAttribute("tokenDto");
+
+            if (tokenDto != null) {
+                httpServletResponse.setHeader(
+                        "Set-Cookie",
+                        String.format(
+                                "refresh-token=%s; Path=/; HttpOnly; Secure; SameSite=None; Partitioned; Max-Age=604800; Domain=ticatch.vercel.app",
+                                tokenDto.getRefreshToken()
+                        )
+                );
+            }
         }
         chain.doFilter(request, response);
     }
