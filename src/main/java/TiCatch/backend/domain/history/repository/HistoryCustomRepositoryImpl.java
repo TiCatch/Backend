ab438921259d56d9fd037dc0c6de5882f0d9031e
kitaee <dlrlxo999@naver.com>
2025-02-11 13:12:03 +0900

    feat: TicketingLevel의 동적 정렬 기준 추가

--- src/main/java/TiCatch/backend/domain/history/repository/HistoryCustomRepositoryImpl.java
+++ src/main/java/TiCatch/backend/domain/history/repository/HistoryCustomRepositoryImpl.java
@@ -1,12 +1,16 @@
 package TiCatch.backend.domain.history.repository;
 
 import TiCatch.backend.domain.history.dto.response.HistoryPagingResponse;
+import TiCatch.backend.domain.ticketing.entity.TicketingLevel;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.support.PageableExecutionUtils;
 
 import java.util.List;
@@ -31,7 +35,8 @@ public class HistoryCustomRepositoryImpl implements HistoryCustomRepository {
                         history.ticketingTime.as("ticketingTime")))
                 .from(history)
                 .where(userIdEq(userId))
-                .orderBy(history.createdDate.desc())
+                .orderBy(getOrderSpecifier(pageable))
+                .limit(pageable.getPageSize())
                 .offset(pageable.getOffset())
                 .fetch();
 
@@ -48,5 +53,28 @@ public class HistoryCustomRepositoryImpl implements HistoryCustomRepository {
 
     private BooleanExpression userIdEq(Long userId) {
         return isNullOrEmpty(String.valueOf(userId)) ? null : history.user.userId.eq(userId);
+    }
+
+    private OrderSpecifier<?> getOrderSpecifier(Pageable pageable) {
+        for (Sort.Order order : pageable.getSort()) {
+            if (order.getProperty().equals("ticketingTime")) {
+                return order.isAscending() ? history.createdDate.asc() : history.createdDate.desc();
+            } else if (order.getProperty().equals("ticketingLevel")) {
+                return order.isAscending()
+                        ? new CaseBuilder()
+                        .when(history.ticketingLevel.eq(TicketingLevel.EASY)).then(0)
+                        .when(history.ticketingLevel.eq(TicketingLevel.NORMAL)).then(1)
+                        .when(history.ticketingLevel.eq(TicketingLevel.HARD)).then(2)
+                        .otherwise(3)
+                        .asc()
+                        : new CaseBuilder()
+                        .when(history.ticketingLevel.eq(TicketingLevel.EASY)).then(0)
+                        .when(history.ticketingLevel.eq(TicketingLevel.NORMAL)).then(1)
+                        .when(history.ticketingLevel.eq(TicketingLevel.HARD)).then(2)
+                        .otherwise(3)
+                        .desc();
+            }
+        }
+        return history.ticketingTime.desc();
     }
 }
