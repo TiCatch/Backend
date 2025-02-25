13638284b51ce6805f26b79ed8d4342aa271336c
kitaee <dlrlxo999@naver.com>
2025-02-25 10:23:11 +0900

    refactor: RestTemplateService 리팩토링

--- src/main/java/TiCatch/backend/domain/payment/service/PaymentService.java
+++ src/main/java/TiCatch/backend/domain/payment/service/PaymentService.java
@@ -2,15 +2,14 @@ package TiCatch.backend.domain.payment.service;
 
 import TiCatch.backend.domain.payment.dto.ApproveResponse;
 import TiCatch.backend.domain.payment.dto.ReadyResponse;
+import TiCatch.backend.global.external.RestTemplateService;
 import lombok.RequiredArgsConstructor;
 import TiCatch.backend.global.constant.PaymentConstants;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,6 +17,8 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class PaymentService {
+
+    private final RestTemplateService restTemplateService;
 
     @Value("${kakaopay.secretKey}")
     private String kakaopaySecretKey;
@@ -30,45 +31,40 @@ public class PaymentService {
 
     //카카오 페이 결제창 연결
     public ReadyResponse ready(String name, int totalPrice){
+        HttpEntity<Map<String, String>> readyRequest = createReadyRequest(name, totalPrice);
+        return restTemplateService.post(PaymentConstants.PAYMENT_READY_URL, readyRequest, ReadyResponse.class).getBody();
+    }
+
+    public ApproveResponse payApprove(String tid, String pgToken){
+        HttpEntity<Map<String, String>> approveRequest = createApproveRequest(tid, pgToken);
+        return restTemplateService.post(PaymentConstants.PAYMENT_APPROVE_URL, approveRequest, ApproveResponse.class).getBody();
+    }
+
+    private HttpEntity<Map<String, String>> createReadyRequest(String name, int totalPrice) {
         Map<String, String> parameters = new HashMap<>();
         parameters.put("cid", cid);
         parameters.put("partner_order_id", PaymentConstants.PARTNER_ORDER_ID);
         parameters.put("partner_user_id", PaymentConstants.PARTNER_USER_ID);
-        parameters.put("item_name" , name);
-        parameters.put("quantity","1");
+        parameters.put("item_name", name);
+        parameters.put("quantity", "1");
         parameters.put("total_amount", String.valueOf(totalPrice));
-        parameters.put("tax_free_amount","0");
+        parameters.put("tax_free_amount", "0");
         parameters.put("approval_url", sampleHost + PaymentConstants.PAYMENT_SUCCESS_URL);
-        parameters.put("cancel_url",sampleHost + PaymentConstants.PAYMENT_CANCEL_URL);
+        parameters.put("cancel_url", sampleHost + PaymentConstants.PAYMENT_CANCEL_URL);
         parameters.put("fail_url", sampleHost + PaymentConstants.PAYMENT_FAIL_URL);
 
-        HttpEntity<Map<String,String>> requestEntity = new HttpEntity<>(parameters, this.getHeaders());
-
-        RestTemplate template = new RestTemplate();
-
-        String url = PaymentConstants.PAYMENT_READY_URL;
-
-        ResponseEntity<ReadyResponse> responseEntity = template.postForEntity(url, requestEntity, ReadyResponse.class);
-        return responseEntity.getBody();
-
+        return new HttpEntity<>(parameters, getHeaders());
     }
 
-    public ApproveResponse payApprove(String tid, String pgToken){
+    private HttpEntity<Map<String, String>> createApproveRequest(String tid, String pgToken) {
         Map<String, String> parameters = new HashMap<>();
-
-        parameters.put("cid",cid);
+        parameters.put("cid", cid);
         parameters.put("tid", tid);
         parameters.put("partner_order_id", PaymentConstants.PARTNER_ORDER_ID);
         parameters.put("partner_user_id", PaymentConstants.PARTNER_USER_ID);
         parameters.put("pg_token", pgToken);
 
-        HttpEntity<Map<String, String>> requestEntity = new HttpEntity<>(parameters, this.getHeaders());
-
-        RestTemplate template = new RestTemplate();
-        String url = PaymentConstants.PAYMENT_APPROVE_URL;
-        ApproveResponse approveResponse = template.postForObject(url,requestEntity, ApproveResponse.class);
-
-        return approveResponse;
+        return new HttpEntity<>(parameters, getHeaders());
     }
 
     private HttpHeaders getHeaders() {
@@ -77,5 +73,4 @@ public class PaymentService {
         headers.setContentType(MediaType.APPLICATION_JSON);
         return headers;
     }
-
 }
