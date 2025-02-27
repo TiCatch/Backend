83d1b64c46375b0a3f53c7cd99e786e1ef801a42
yj0111 <ooyouj@naver.com>
2025-02-27 17:45:20 +0900

    add: UserResponseDto에 fromEntity 추가

--- src/main/java/TiCatch/backend/domain/user/dto/response/UserResponseDto.java
+++ src/main/java/TiCatch/backend/domain/user/dto/response/UserResponseDto.java
@@ -1,7 +1,7 @@
 package TiCatch.backend.domain.user.dto.response;
 
+import TiCatch.backend.domain.user.entity.User;
 import lombok.*;
-
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,6 +11,15 @@ public class UserResponseDto {
     private Long userId;
     private String userNickname;
     private int userScore;
-    private String credentialId;
     private String userEmail;
+
+    // 🔹 엔티티 -> DTO 변환을 담당하는 static 메서드 추가
+    public static UserResponseDto fromEntity(User user) {
+        return UserResponseDto.builder()
+                .userId(user.getUserId())
+                .userNickname(user.getUserNickname())
+                .userScore(user.getUserScore())
+                .userEmail(user.getEmail())
+                .build();
+    }
 }
