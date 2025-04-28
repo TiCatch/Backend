19a05a0590f4c2c7ce50eac109d68e1180e72be1
kitaee <dlrlxo999@naver.com>
2025-04-28 19:32:13 +0900

    fix: BATCH_SIZE 감소

--- src/main/java/TiCatch/backend/global/constant/SchedulerConstants.java
+++ src/main/java/TiCatch/backend/global/constant/SchedulerConstants.java
@@ -3,6 +3,6 @@ package TiCatch.backend.global.constant;
 public class SchedulerConstants {
     public static final int DYNAMIC_SCHEDULER_INITIAL_DELAY = 3;
     public static final int DYNAMIC_SCHEDULER_PERIOD = 10;
-    public static final int BATCH_SIZE = 300;
+    public static final int BATCH_SIZE = 1;
     public static final long TICKETING_SCHEDULER_PERIOD = 1000L;
 }
